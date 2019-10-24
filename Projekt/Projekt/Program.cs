533a7c07c65200697326961f90ed168ded695d2a
Strackninja <strahinja.velickovic018@gmail.com>
2019-10-24 16:29:37 +0200

    MyPlayer Projekt
    Har bytt från matlagning spelet till att göra ett program likt funktionen MyPlayer i NBA2k20.

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -10,6 +10,17 @@ namespace Projekt
     {
         static void Main(string[] args)
         {
+
+            // idé till spel
+            // Du ska skapa din egna basketspelare likt Myplayer funktionen i alla NBA2K spel. 
+            // Du ska välja olika atributer till din karaktär, alltså t.ex Position , Length , Weight , Playstyle och Takeover
+
+            // För Position ska du kunna välja mellan PointGuard, Shooting Guard, Small Forward, Power Forward och Center
+            // För Lenght ska du kunna välja mellan att vara 5'7-7'4
+            // För Weight ska du kunna välja mellan 175lb-280lb
+            // För Playstyle ska du kunna välja mellan många men de jag har kommit på nu är, shooting, playmaking, perimiter defence och inner defence.
+            // För Takeover ska man kunna välja mellan Slashing (Dunking), 
+
         }
     }
 }
